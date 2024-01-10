b8f66f8530fec5344db538c10f8a9be33a9a80a6
Taklit Dina <ez_taklit@esi.dz>
2024-01-10 23:10:50 +0100

    feat: fix linting issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,31 @@
 module.exports = {
-  extends: ["react-app", "react-app/jest", "airbnb", "prettier"],
-  plugins: ["prettier"],
+  extends: ['react-app', 'react-app/jest', 'airbnb', 'prettier'],
+  plugins: ['prettier'],
   rules: {
     // Enable perttier rules
-    "prettier/prettier": "error",
+    'prettier/prettier': 'error',
     // React scope no longer necessary with new JSX transform
-    "react/react-in-jsx-scope": "off",
+    'react/react-in-jsx-scope': 'off',
     // Allow .js files to use JSX syntax
-    "react/jsx-filename-extension": ["error", { extensions: [".js", ".jsx"] }],
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
     // Allow  jsx spread opreator
-    "react/jsx-props-no-spreading": "off",
+    'react/jsx-props-no-spreading': 'off',
     // Make default export not mandatory
-    "import/prefer-default-export": "off",
+    'import/prefer-default-export': 'off',
     // Don't require defaultProps
-    "react/require-default-props": "off",
+    'react/require-default-props': 'off',
     // Allow object in propTpyes
-    "react/forbid-prop-types": "off",
+    'react/forbid-prop-types': 'off',
     // allow unamed function components
-    "react/function-component-definition": [
+    'react/function-component-definition': [
       2,
       {
-        namedComponents: "function-declaration",
-        unnamedComponents: "arrow-function",
+        namedComponents: 'function-declaration',
+        unnamedComponents: 'arrow-function',
       },
     ],
     // Allow 'react/no-unescaped-entitie'
-    "react/no-unescaped-entities": "off",
-    "@next/next/no-page-custom-font": "off",
+    'react/no-unescaped-entities': 'off',
+    '@next/next/no-page-custom-font': 'off',
   },
-};
+}
